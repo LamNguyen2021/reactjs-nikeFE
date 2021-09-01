3d653f670cce646c91133357639bd98e922335f7
LamNguyen <9.4ngoclam@gmail.com>
2021-09-01 20:03:03 +0700

    add notification for log out

--- src/Layout/Navbar/NavSub.tsx
+++ src/Layout/Navbar/NavSub.tsx
@@ -9,6 +9,8 @@ import {
   setToken,
   setUserInfo,
 } from "../../Redux/credentials/credentialsReducer";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -67,6 +69,11 @@ export default function NavSub() {
     dispatch(setIsLogin(false));
     dispatch(setToken(""));
     dispatch(setUserInfo({}));
+
+    toast.dark("GOOD BYE 😭", {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 2500,
+    });
   };
 
   return (
@@ -90,6 +97,7 @@ export default function NavSub() {
           </>
         )}
       </div>
+      <ToastContainer />
     </AppBar>
   );
 }
